f995806f9ae88d6097d6ac2441caf47a1165e6ff
NickTzo <nikostzortziss@gmail.com>
2023-06-15 23:08:31 +0300

    BankApp v0.1

---
+++ model/IdentifiableEntity.java
@@ -0,0 +1,20 @@
+package gr.aueb.cf.model;
+
+public class IdentifiableEntity {
+    private long id;
+
+//    public IdentifiableEntity() {
+//    }
+//
+//    public IdentifiableEntity(long id) {
+//        this.id = id;
+//    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
